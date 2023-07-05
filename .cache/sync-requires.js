0a4b2869a3d1d529001ec319fca880155ea282cc
EMar42 <36295473+EMar42@users.noreply.github.com>
2023-07-05 21:02:51 +0300

    fix cache on app.js

---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("D:\\Dev\\test\\Web-Theme\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("D:\\Dev\\test\\Web-Theme\\src\\pages\\404.js")),
+  "component---src-pages-about-js": preferDefault(require("D:\\Dev\\test\\Web-Theme\\src\\pages\\about.js")),
+  "component---src-pages-blog-js": preferDefault(require("D:\\Dev\\test\\Web-Theme\\src\\pages\\blog.js")),
+  "component---src-pages-careers-js": preferDefault(require("D:\\Dev\\test\\Web-Theme\\src\\pages\\careers.js")),
+  "component---src-pages-contact-js": preferDefault(require("D:\\Dev\\test\\Web-Theme\\src\\pages\\contact.js")),
+  "component---src-pages-index-js": preferDefault(require("D:\\Dev\\test\\Web-Theme\\src\\pages\\index.js")),
+  "component---src-pages-pricing-js": preferDefault(require("D:\\Dev\\test\\Web-Theme\\src\\pages\\pricing.js"))
+}
+
